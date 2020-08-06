cd2fe407e83fdbe2f66187a8207fb92d706fcfc1
Feeroz Alam <feeroz123@gmail.com>
2020-08-07 00:51:00 +0530

    Added for Test Rail integration

---
+++ src/test/java/constants/Statuses.java
@@ -0,0 +1,15 @@
+package constants;
+
+/**
+ * Contains the list of possible Test Statuses for a Test Case from Test Rail
+ * @author kickdrum
+ *
+ */
+public class Statuses {
+	// Test Status mapping-> 1-Passed, 2-Blocked, 3-Untested, 4-Retest, 5-Failed
+	public final int TC_PASSED_STATUS = 1;
+	public final int TC_BLOCKED_STATUS = 2;
+	public final int TC_UNTESTED_STATUS = 3;
+	public final int TC_RETEST_STATUS = 4;
+	public final int TC_FAILED_STATUS = 5;
+}
