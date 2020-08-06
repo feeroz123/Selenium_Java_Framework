d2de77867c8d3f690b921ca89145fdbfd349f521
Feeroz Alam <feeroz123@gmail.com>
2020-08-07 00:59:32 +0530

    Added imports for Test Rail integration

--- src/test/java/utilities/Utilities.java
+++ src/test/java/utilities/Utilities.java
@@ -15,6 +15,7 @@ import java.util.Random;
 import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.simple.JSONObject;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
